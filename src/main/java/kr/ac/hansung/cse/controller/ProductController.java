6817faa1360093bbe3a4813503cbc17d445ce599
ji_hyeon <lovely2270@naver.com>
2020-06-14 23:50:09 +0900

    Create project and Create Model

---
+++ src/main/java/kr/ac/hansung/cse/controller/ProductController.java
@@ -0,0 +1,22 @@
+package kr.ac.hansung.cse.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ProductController {
+
+	//Add(POST)
+	
+	//Get(GET) - 모든 Product
+	
+	//Get(GET) - id로 해당 Product
+	
+	//Get(GET) - 해당 category를 가진 product
+	
+	//Update(PUT)
+	
+	//Delete(DELETE)
+
+}
---
+++ src/main/java/kr/ac/hansung/cse/hw3helloProductApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.hansung.cse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class hw3helloProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(hw3helloProductApplication.class, args);
+	}
+
+}
